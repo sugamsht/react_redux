5872e9ec348998c2b87b6d393aa952fc73fb0758
Sugam Shrestha <sugamsht143@gmail.com>
2022-05-31 00:34:39 +0545

    using memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,18 @@
 import jsonPlaceHolder from "../api/jsonPlaceHolder"
+import _ from "lodash"
+import React, { useMemo } from "react";
 
 //action creator
-export const fetchPosts = () => {
-    return async (dispatch) => {
-        const response = await jsonPlaceHolder.get('/posts');
-        dispatch({ type: 'FETCH_POSTS', payload: response.data });
-    }
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceHolder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 }
 
-export const fetchUser = (id) => async dispatch => {
+
+export const fetchUser = (id) => dispatch => _fetchedUser(id, dispatch);
+
+//memoized function
+const _fetchedUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: response.data });
-}
-
+})
